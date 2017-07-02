d3bd06aca6dbaea50fe8de36dec93493b08d08ef
cyclic-ester <Cyclic_Ester@yahoo.co.jp>
2017-07-03 00:21:05 +0900

    [fix] main file

--- src/main/java/life/grass/grasshousing/GrassHousing.java
+++ src/main/java/life/grass/grasshousing/GrassHousing.java
@@ -1,13 +1,20 @@
 package life.grass.grasshousing;
 
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
 import life.grass.grasshousing.event.ChestLockEvent;
+import life.grass.grasshousing.protocol.WindowPacketRenamer;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class GrassHousing extends JavaPlugin {
+    private static ProtocolManager protocolManager;
 
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new ChestLockEvent(), this);
+
+        protocolManager = ProtocolLibrary.getProtocolManager();
+        WindowPacketRenamer.getInstance().addListener(protocolManager, this);
     }
 
     @Override
